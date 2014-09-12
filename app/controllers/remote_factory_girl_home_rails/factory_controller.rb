acc3251b36e249ca477d46b6958d48904628fb89
Chris Bonser <chris@khorus.com>
2014-09-12 11:28:24 -0500

    Refactoring a few methods to slim down some methods

--- app/controllers/remote_factory_girl_home_rails/factory_controller.rb
+++ app/controllers/remote_factory_girl_home_rails/factory_controller.rb
@@ -1,14 +1,13 @@
 module RemoteFactoryGirlHomeRails
   class FactoryController < ApplicationController
     skip_before_filter *RemoteFactoryGirlHomeRails.skip_before_filter
+    around_filter :ensure_enabled
     around_filter :provide_clean_errors, only: [:create, :attributes_for]
 
     def create
-      #provide_clean_errors do
-        Rails.logger.debug "FactoryGirl.create( #{factory(params)}, #{attributes(params).inspect})"
-        factory = FactoryGirl.create(factory(params), attributes(params))
-        render json: factory
-      #end
+      Rails.logger.debug "FactoryGirl.create( #{factory}, #{attributes.inspect})"
+      factory_item = FactoryGirl.create(factory, attributes)
+      render json: factory_item
     end
 
     def index
@@ -17,38 +16,37 @@ module RemoteFactoryGirlHomeRails
     end
 
     def attributes_for
-      #provide_clean_errors do
-        Rails.logger.debug "FactoryGirl.attributes_for( #{factory(params)}, #{attributes(params).inspect})"
-        factory = FactoryGirl.attributes_for(factory(params), attributes(params))
-        render json: {factory(params) => factory}, serializer: nil
-      #end
+      Rails.logger.debug "FactoryGirl.attributes_for( #{factory}, #{attributes.inspect})"
+      attrs = FactoryGirl.attributes_for(factory, attributes)
+      render json: {factory => attrs}, serializer: nil
     end
 
     private
 
-    def factory(params)
+    def factory
       params['factory'].to_sym
     end
 
-    def attributes(params)
+    def attributes
       params['attributes'] || {}
     end
 
     def provide_clean_errors
+      yield
+    rescue StandardError => e
+      Rails.logger.debug e.to_s
+      Rails.logger.debug e.class
+      #e.backtrace.each{|t| Rails.logger.debug t }
+      render json: {errors: [{factory: factory, message: e.to_s}] }, status: 422, serializer: nil
+    end
+
+    def ensure_enabled
       if RemoteFactoryGirlHomeRails.enabled?
-        begin
-          yield
-        rescue StandardError => e
-          Rails.logger.debug e.to_s
-          Rails.logger.debug e.class
-          #e.backtrace.each{|t| Rails.logger.debug t }
-          render json: {errors: [{factory: factory(params), message: e.to_s}] }, status: 422, serializer: nil
-        end
+        yield
       else
         forbidden = 403
         render json: { status: forbidden }, status: forbidden, serializer: nil
       end
-
     end
   end
 end
